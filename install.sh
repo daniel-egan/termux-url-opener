96bc0ef4b07e09bf6dfe482d9f8d2c4afb12af73
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2023-01-04 16:23:23 +0000

    Removed Auto-editor and Numpy

--- install.sh
+++ install.sh
@@ -4,9 +4,7 @@ BLUE='\e[34m'
 NC='\e[0m'
 
 pkg install python ffmpeg
-pkg install python-numpy -y
 pip install -U yt-dlp
-pip install auto-editor
 mkdir ~/storage/music/Podcasts/
 mkdir ~/storage/music/Music/
 mkdir ~/storage/movies/Videos/

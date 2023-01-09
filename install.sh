5183dbf77c0a31f3f65b8476ffbd1dfacc7292c6
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2023-01-09 14:54:34 +0000

    Added mkdir

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ pip install -U yt-dlp -y
 mkdir ~/storage/music/Podcasts/
 mkdir ~/storage/music/Music/
 mkdir ~/storage/movies/Videos/
+mkdir ~/bin/
 mv termux-url-opener ~/bin/
 
 echo -e "${BLUE}Setup is complete.${NC}\n\n"

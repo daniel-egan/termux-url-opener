bf853958dc7213ab07a6ad1f768947c7cc36efa5
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2023-01-09 14:48:11 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 BLUE='\e[34m'
 NC='\e[0m'
 
-pkg install python ffmpeg
-pip install -U yt-dlp
+pkg install python ffmpeg -y
+pip install -U yt-dlp -y
 mkdir ~/storage/music/Podcasts/
 mkdir ~/storage/music/Music/
 mkdir ~/storage/movies/Videos/
